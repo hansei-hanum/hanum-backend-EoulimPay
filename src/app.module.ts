0b158032f0e62c5369953434e6d890c7b1209cb6
leeyeonwoo <rexrex1105@gmail.com>
2024-09-22 23:41:01 +0900

    fix: edit run scripts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,26 @@ import { BalanceService } from './EoullimBalance/balance.service';
 import { BalanceController } from './EoullimBalance/Balance.controller';
 import { RepositoryModule } from './repository/repository.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { ClientsModule } from '@nestjs/microservices';
+import { grpcClientOptions } from './micro/grpcClientOptions';
 
 @Module({
   imports: [
     PrismaModule,
     BalanceModule,
     RepositoryModule,
+    AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
+    ClientsModule.register([
+      {
+        name: 'AUTH_PACKAGE',
+        ...grpcClientOptions,
+      },
+    ]),
   ],
   controllers: [BalanceController],
   providers: [BalanceService],
